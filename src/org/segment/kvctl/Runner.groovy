9b606e89eaffcba17dda809f03f6d1febf28521b
kerry <dingyong87@163.com>
2026-03-03 12:53:21 +0800

    feat: update ddl for new h2 version.

--- src/org/segment/kvctl/Runner.groovy
+++ src/org/segment/kvctl/Runner.groovy
@@ -75,17 +75,20 @@ create table migrate_tmp_save (
 create index idx_migrate_tmp_save_app_id on migrate_tmp_save(app_id);
 
 create table leaf_alloc (
-  biz_tag varchar(128)  NOT NULL DEFAULT '',
-  max_id bigint(20) NOT NULL DEFAULT '1',
-  step int(11) NOT NULL,
-  description varchar(256)  DEFAULT NULL,
-  update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (biz_tag)
+  biz_tag varchar(128) primary key,
+  max_id bigint,
+  step int,
+  description varchar(256),
+  update_time timestamp default current_timestamp
 );
 '''
     ddl.trim().split(';').each {
+        def sql0 = it.toString().trim()
+        println '--- to execute: '
+        println sql0
+        println '---'
         try {
-            d.exe(it.toString())
+            d.exe(sql0)
         } catch (Exception e) {
             log.error('create table fail, ex: ' + e.message)
         }
